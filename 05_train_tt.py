5fdeb1ec87330dd2d745c9828fbbc114b66b02f8
Aparna <aparna@Aparnas-MacBook-Pro.local>
2025-06-20 11:30:48 +0100

    done

--- 05_train_tt.py
+++ 05_train_tt.py
@@ -8,6 +8,22 @@ import wandb
 import tqdm
 from load_glove import load_glove
 
+#Hyperparameters
+EMBED_DIM = 100 
+Batchsize = 256
+Margin = 0.3
+lr = 0.003
+Epochs = 3
+
+wandb.init(project='mlx6-week-02-two',
+           config={
+             "num_epochs": Epochs,
+             "batch_size": Batchsize,
+             "learning_rate": lr,
+             "margin": Margin,
+             "embedding_dim": EMBED_DIM,
+           })
+
 # Load tokenizer
 with open('./corpus/tokeniser.pkl', 'rb') as f:
     tkns = pickle.load(f)
@@ -15,29 +31,26 @@ words_to_idx, ids_to_words = tkns['words_to_idx'], tkns['ids_to_words']
 dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load GloVe embeddings
-
-EMBED_DIM = 100  # match your GloVe file!
 glove_path = '/Users/aparna/Documents/CollabWeek2/MLX_two_towers/corpus/glove.6B.100d.txt'
 embeddings = load_glove(glove_path, words_to_idx, embedding_dim=EMBED_DIM)
 embedding_layer = nn.Embedding.from_pretrained(embeddings, freeze=True)
 
 # Dataset and DataLoader
 ds = dataset.Triplets(embedding_layer, words_to_idx)
-dl = torch.utils.data.DataLoader(ds, batch_size=256, shuffle=True)
+dl = torch.utils.data.DataLoader(ds, Batchsize, shuffle=True)
 
 # Towers model
 ts = datetime.datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
 two = models.Towers(glove_dim=EMBED_DIM).to(dev)
 torch.save(two.state_dict(), f'./checkpoints/{ts}.0.0.two.pth')
 print('two:', sum(p.numel() for p in two.parameters()))
-opt = torch.optim.Adam(two.parameters(), lr=0.003)
-wandb.init(project='mlx6-week-02-two')
+opt = torch.optim.Adam(two.parameters(), lr=ProcessLookupError)
 
-for epoch in range(1):
+for epoch in range(Epochs):
     prgs = tqdm.tqdm(dl, desc=f"Epoch {epoch + 1}", leave=False)
     for idx, (qry, pos, neg) in enumerate(prgs):
         qry, pos, neg = qry.to(dev), pos.to(dev), neg.to(dev)
-        loss = two(qry, pos, neg, mrg=0.3)
+        loss = two(qry, pos, neg, mrg=Margin)
         opt.zero_grad()
         loss.backward()
         opt.step()
@@ -46,3 +59,5 @@ for epoch in range(1):
             torch.save(two.state_dict(), f'./checkpoints/{ts}.{epoch}.{idx}.two.pth')
 
 wandb.finish()
+
+
